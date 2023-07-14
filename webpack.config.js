c4d3a0a658c8d7b094baa38f1d7e2cdb44bc5cae
Mehmet Selçuk Güler <gercekselcukguler@gmail.com>
2023-07-14 13:33:35 +0300

    Install HtmlWebpack Plugin
    Run Test (succes)

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
   mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+  ],
   output: {
-    filename: 'main.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
   },
 };
