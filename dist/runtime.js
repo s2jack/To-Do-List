70cc5c66eec6f0da485da16900f171397038b9a4
Mehmet Selçuk Güler <gercekselcukguler@gmail.com>
2023-07-14 19:40:03 +0300

    Create tasklist section
    Create blueprint of taskboard
    Add Task template
    Add Header
    Add footer
    Add heading
    Add text input
    Give basic styles

--- dist/runtime.js
+++ dist/runtime.js
@@ -182,7 +182,7 @@
 /******/ 			return __webpack_require__.O(result);
 /******/ 		}
 /******/ 		
-/******/ 		var chunkLoadingGlobal = self["webpackChunkproject_template_01"] = self["webpackChunkproject_template_01"] || [];
+/******/ 		var chunkLoadingGlobal = self["webpackChunkTo_Do_List"] = self["webpackChunkTo_Do_List"] || [];
 /******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
 /******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
 /******/ 	})();
