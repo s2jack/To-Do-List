6261216792b6df0b52c638996ad32156d720abba
Mehmet Selçuk Güler <gercekselcukguler@gmail.com>
2023-07-15 21:59:47 +0300

    Set-up modular structure
    Add addTask.js file to control adding new element
    Add removeTask.js file to control removing task elements
    Modify taskClass
    Add new property 'id' for every uniqe element
    Fix style issues
    Fix linters

--- modules/tasksUI.js
+++ modules/tasksUI.js
@@ -1,11 +1,14 @@
 import _ from 'lodash'; // eslint-disable-line no-unused-vars
-import Task from './taskClass.js';
+// import Task from './taskClass.js';
 
 export default class TasksUI {
   static checkData = () => {
     // console.log('checking database')
     // console.log(localStorage.getItem('tasksDB') == '[]')
-    if (localStorage.getItem('tasksDB') === null || localStorage.getItem('tasksDB') === '[]') {
+    if (
+      localStorage.getItem('tasksDB') === null
+      || localStorage.getItem('tasksDB') === '[]'
+    ) {
       // console.log(localStorage.getItem('tasksDB'));
       return false;
     }
@@ -32,80 +35,97 @@ export default class TasksUI {
         .forEach((task) => {
           // console.log(task);
           const taskContainer = document.createElement('div');
-          taskContainer.className = 'list-element';
-          taskContainer.id = 'task-container';
+          taskContainer.className = 'list-element flex';
+          taskContainer.id = `${task.id}`;
           tasksUIelement.appendChild(taskContainer);
           const taskToogle = document.createElement('input');
           taskToogle.type = 'checkbox';
           taskToogle.className = 'task';
-          taskToogle.id = `${task.index}`;
+          taskToogle.checked = false;
+          taskToogle.id = `toogle${task.id}`;
           taskContainer.appendChild(taskToogle);
           const taskTitle = document.createElement('input');
           taskTitle.className = 'task-title-text';
-          taskTitle.id = `${task.index}`;
+          if (task.completed) {
+            taskToogle.checked = true;
+            taskTitle.style = 'text-decoration-line: line-through;';
+          } else {
+            taskToogle.checked = false;
+            taskTitle.style = '';
+          }
+          taskTitle.id = `title${task.id}`;
           taskTitle.value = task.description;
           taskContainer.appendChild(taskTitle);
 
-          taskTitle.addEventListener('keypress', (event) => {
-            if (event.key === 'Enter') {
+          taskTitle.addEventListener('change', (event) => {
+            if (taskTitle.value !== task.description) {
               event.preventDefault();
               // console.log('pressed Enter');
               this.editTask(event);
             }
           });
-          taskToogle.addEventListener('change', this.removeTask);
+
+          taskToogle.addEventListener('change', (event) => {
+            // console.log(event);
+            // console.log(event.target.id);
+            // const order = event.target.id - 1
+            const dBInstance = JSON.parse(localStorage.getItem('tasksDB'));
+            // console.log(dBInstance);
+            const elementID = document.querySelector(`#${event.target.id}`)
+              .parentElement.id;
+            const taskElement = dBInstance.find(
+              (elem) => elem.id === elementID,
+            );
+            // console.log(taskElement);
+            if (taskElement.completed) {
+              // console.log('checking task is complete');
+              taskToogle.checked = false;
+              taskElement.completed = false;
+              localStorage.setItem('tasksDB', JSON.stringify(dBInstance));
+              TasksUI.loadTasksUI();
+            } else {
+              taskToogle.checked = true;
+              taskElement.completed = true;
+              localStorage.setItem('tasksDB', JSON.stringify(dBInstance));
+              TasksUI.loadTasksUI();
+            }
+          });
         });
     }
   };
 
-  static addTask() {
-    // console.log('event works');
-    const dB = JSON.parse(localStorage.getItem('tasksDB')) || [];
-    // console.log(dB);
-    const addTaskInput = document.querySelector('#task-input');
-    if (addTaskInput.value) {
-      // console.log('input filled');
-      // console.log(addTaskInput.value);
-      // console.log('database empty');
-      const index = dB.length + 1;
-      const task = new Task(addTaskInput.value, index);
-      const newDB = [...dB, task];
-      localStorage.setItem('tasksDB', JSON.stringify(newDB));
-      TasksUI.checkData();
-      TasksUI.loadTasksUI();
-      /* } else {
-         console.log('database has value');
-       } */
-    }
-  }
-
-  static removeTask(event) {
-    const tasksDbInstance = JSON.parse(localStorage.getItem('tasksDB')) || [];
-    const order = parseInt(event.target.id, 10) - 1;
-    tasksDbInstance.splice(order, 1);
-    localStorage.setItem('tasksDB', JSON.stringify(tasksDbInstance));
-    tasksDbInstance.forEach((element) => {
-      // console.log(element)
-      if (element.index - 1 >= order) {
-        element.index -= 1;
-        // console.log(element.index);
-      }
-    });
-    localStorage.setItem('tasksDB', JSON.stringify(tasksDbInstance));
-    TasksUI.loadTasksUI();
-  }
+  // static removeTask(event) {
+  //   const tasksDbInstance = JSON.parse(localStorage.getItem('tasksDB')) || [];
+  //   const order = parseInt(event.target.id, 10) - 1;
+  //   tasksDbInstance.splice(order, 1);
+  //   localStorage.setItem('tasksDB', JSON.stringify(tasksDbInstance));
+  //   tasksDbInstance.forEach((element) => {
+  //     // console.log(element)
+  //     if (element.index - 1 >= order) {
+  //       element.index -= 1;
+  //       // console.log(element.index);
+  //     }
+  //   });
+  //   localStorage.setItem('tasksDB', JSON.stringify(tasksDbInstance));
+  //   TasksUI.loadTasksUI();
+  // }
 
   static editTask(event) {
+    // console.log('edited');
     const tasksDbInstance = JSON.parse(localStorage.getItem('tasksDB')) || [];
     // console.log(tasksDbInstance);
-    // console.log(event.target);
-    const order = parseInt(event.target.id, 10) - 1;
+    // console.log(event.target.id);
+    // const order = parseInt(event.target.id, 10) - 1;
     // console.log(order);
     // console.log(tasksDbInstance[order]);
-    const elementInput = document.getElementsByClassName('task-title-text')[order];
-    tasksDbInstance[order].description = elementInput.value;
+    const elementInput = document.querySelector(`#${event.target.id}`);
+    const elementID = elementInput.parentElement.id;
+    // console.log(elementID);
+    const element = tasksDbInstance.find((elem) => elem.id === elementID);
     // console.log(tasksDbInstance[order]);
+    element.description = elementInput.value;
     localStorage.setItem('tasksDB', JSON.stringify(tasksDbInstance));
     // console.log(tasksDbInstance);
+    TasksUI.loadTasksUI();
   }
 }
